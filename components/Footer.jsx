4155b2f70d2e2aa6f1a84ba9fdd2321bea5f013f
Mwia <iansnr10@gmail.com>
2025-11-11 13:22:43 +0300

    editing the navbar and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer(){
     return(
-        <footer>
+        <footer className='flex flex-col bg-[#18312b] p-2 justify-between text-white text-center'>
             <p>&copy; 2025 Afya Care</p>
+            <p>Disclaimer: We are not a heath crisis emergency platform</p>
         </footer>
     )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,13 +2,17 @@ import { Link } from 'react-router-dom'
 export default function Navbar(){
     return(
         <>
-        <nav className='flex bg-[#18312b] p-2'>
+        <nav className='flex bg-[#18312b] p-2 items-center justify-between'>
             <div>
                 <Link to='/'>
-                <h1 className='text-4xl font-bold text-zinc-200'>Afya Care</h1>
+                <h1 className='text-4xl font-bold text-zinc-200 cursor-pointer'>Afya Care</h1>
                 </Link>
             </div>
+            <div className='text-white gap-3 flex space-x-6'>
             <Link to='/about'>About Us</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>SignUp</Link>
+            </div>
         </nav>
         </>
     )
