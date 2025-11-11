60b9ec7d27f8cc8bbcd5a0abf3db3e3f3ebcce71
Mwia <iansnr10@gmail.com>
2025-11-11 16:04:14 +0300

    creating a mock api

---
+++ api/doctorsApi.js
@@ -0,0 +1,300 @@
+//Images
+import doctor1 from './images/doctor-1.jpeg'
+import doctor2 from './images/doctor-2.jpeg'
+import doctor3 from './images/doctor-3.jpeg'
+import doctor4 from './images/doctor-4.jpeg'
+import doctor5 from './images/doctor-5.jpeg'
+import doctor6 from './images/doctor-6.avif'
+import doctor7 from './images/doctor-7.avif'
+import doctor8 from './images/doctor-8.avif'
+import doctor9 from './images/doctor-9.avif'
+import doctor10 from './images/doctor-10.avif'
+import doctor11 from './images/doctor-11.avif'
+import doctor12 from './images/doctor-12.avif'
+import doctor13 from './images/doctor-13.avif'
+import doctor14 from './images/doctor-14.avif'
+import doctor15 from './images/doctor-15.avif'
+import doctor16 from './images/doctor-16.avif'
+import doctor17 from './images/doctor-17.avif'
+import doctor18 from './images/doctor-18.avif'
+import doctor19 from './images/doctor-19.avif'
+import doctor20 from './images/doctor-20.avif'
+import doctor21 from './images/doctor-21.webp'
+
+//Api
+export const doctors = [
+    {
+        id: '1',
+        name: 'Dr. Alice K Mwangi',
+        specialty: 'Cardiology',
+        practice: 'Mental Health',
+        hospital: 'Nairobi Heart Centre',
+        work_experience: 10,
+        price: 2500,
+        image: doctor1,
+        county: 'Nairobi',
+        contact: '+2547839403829'
+
+    },
+    {
+        id: '2',
+        name: 'Dr. Mark Kamau',
+        specialty: 'Psychiatry',
+        practice: 'Healthcare',
+        hospital: 'Kilifi Hospital',
+        work_experience: 14,
+        price: 2500,
+        image: doctor11,
+        county: 'Kilifi',
+        contact: '+2547893003829'
+
+    },
+    {
+        id: '3',
+        name: 'Dr. Michael Otieno',
+        specialty: 'Neuropsychiatry',
+        practice: 'Mental Health',
+        hospital: 'Nakuru Hospital',
+        work_experience: 6,
+        price: 7000,
+        image: doctor12,
+        county: 'Nakuru',
+        contact: '+2547804401029'
+
+    },
+    {
+        id: '4',
+        name: 'Dr. Mary Chebet',
+        specialty: 'Cardiology',
+        practice: 'Healthcare',
+        hospital: 'Lamu Hospital',
+        work_experience: 6,
+        price: 7000,
+        image: doctor2,
+        county: 'Lamu',
+        contact: '+2547003401029'
+
+    },
+    {
+        id: '5',
+        name: 'Dr. Esther Njeri',
+        specialty: 'Counseling Psychology',
+        practice: 'Mental Health',
+        hospital: 'Kenyatta Hospital',
+        work_experience: 10,
+        price: 2500,
+        image: doctor3,
+        county: 'Nairobi',
+        contact: '+2547839432029'
+
+    },
+    {
+        id: '6',
+        name: 'Dr. Alex Mwangi',
+        specialty: 'Counseling Psychology',
+        practice: 'Mental Health',
+        hospital: 'Kisumu Hospital',
+        work_experience: 14,
+        price: 3500,
+        image: doctor13,
+        county: 'Kisumu',
+        contact: '+2547800401029'
+
+    },
+    {
+        id: '7',
+        name: 'Dr. Jackson Kibet',
+        specialty: 'Pediatrics',
+        practice: 'Healthcare',
+        hospital: 'Nakuru Hospital',
+        work_experience: 10,
+        price: 10000,
+        image: doctor14,
+        county: 'Nakuru',
+        contact: '+2547839401029'
+
+    },
+    {
+        id: '8',
+        name: 'Dr. Mary Mutheu',
+        specialty: 'Dermatology',
+        practice: 'Healthcare',
+        hospital: 'Naivasha Hospital',
+        work_experience: 4,
+        price: 2000,
+        image: doctor4,
+        county: 'Naivasha',
+        contact: '+2547839401089'
+
+    },
+    {
+        id: '9',
+        name: 'Dr. Njuguna Achieng',
+        specialty: 'Mental Health Nursing',
+        practice: 'Mental Health',
+        hospital: 'Nyeri Hospital',
+        work_experience: 6,
+        price: 3000,
+        image: doctor15,
+        county: 'Nyeri',
+        contact: '+2547831231029'
+
+    },
+    {
+        id: '10',
+        name: 'Dr. Stella Florence',
+        specialty: 'Cardiology',
+        practice: 'Healthcare',
+        hospital: 'Narok Hospital',
+        work_experience: 16,
+        price: 14000,
+        image: doctor5,
+        county: 'Narok',
+        contact: '+2547839401011'
+
+    },
+    {
+        id: '11',
+        name: 'Dr. James Mali',
+        specialty: 'Psychiatry',
+        practice: 'Mental Health',
+        hospital: 'Good Life Hospital',
+        work_experience: 6,
+        price: 7000,
+        image: doctor16,
+        county: 'Nairobi',
+        contact: '+2547149401029'
+
+    },
+    {
+        id: '12',
+        name: 'Dr. Ruth Joy',
+        specialty: 'Respiratory Medicine',
+        practice: 'Healthcare',
+        hospital: 'Kitui Hospital',
+        work_experience: 12,
+        price: 9000,
+        image: doctor6,
+        county: 'Kitui',
+        contact: '+2547372001029'
+
+    },
+    {
+        id: '13',
+        name: 'Dr. Ken',
+        specialty: 'Pediatrics',
+        practice: 'Healthcare',
+        hospital: 'Aga Khan Hospital',
+        work_experience: 10,
+        price: 11500,
+        image: doctor17,
+        county: 'Nairobi',
+        contact: '+2547123401029'
+
+    },
+    {
+        id: '14',
+        name: 'Dr. James Michael',
+        specialty: 'Cardiology',
+        practice: 'Healthcare',
+        hospital: 'Nairobi Hospital',
+        work_experience: 26,
+        price: 17000,
+        image: doctor18,
+        county: 'Nairobi',
+        contact: '+2547949007290'
+
+    },
+    {
+        id: '15',
+        name: 'Dr. Diana Kamau',
+        specialty: 'Mental Health Nursing',
+        practice: 'Mental Health',
+        hospital: 'Thika Hospital',
+        work_experience: 16,
+        price: 8000,
+        image: doctor7,
+        county: 'Thika',
+        contact: '+2547367101029'
+
+    },
+    {
+        id: '16',
+        name: 'Dr. Anne Wairimu',
+        specialty: 'Counseling Psychology',
+        practice: 'Mental Health',
+        hospital: 'Nairobi Hospital',
+        work_experience: 9,
+        price: 4000,
+        image: doctor8,
+        county: 'Nakuru',
+        contact: '+2547839407869'
+
+    },
+    {
+        id: '17',
+        name: 'Dr. George Mugo',
+        specialty: 'Psychiatry',
+        practice: 'Mental Health',
+        hospital: 'Nairobi Hospital',
+        work_experience: 4,
+        price: 2500,
+        image: doctor19,
+        county: 'Nairobi',
+        contact: '+2547837401029'
+
+    },
+    {
+        id: '18',
+        name: 'Dr. Aisha Ahmed',
+        specialty: 'Cardiology',
+        practice: 'Healthcare',
+        hospital: 'Saidika Hospital',
+        work_experience: 16,
+        price: 9000,
+        image: doctor9,
+        county: 'Kwale',
+        contact: '+2547837201029'
+
+    },
+    {
+        id: '19',
+        name: 'Dr. Simon Karanja',
+        specialty: 'Psychiatry',
+        practice: 'Mental Health',
+        hospital: 'Nakuru Hospital',
+        work_experience: 6,
+        price: 7000,
+        image: doctor20,
+        county: 'Nakuru',
+        contact: '+2547839401098'
+
+    },
+    {
+        id: '20',
+        name: 'Dr. Ruth Odhiambo',
+        specialty: 'Cardiology',
+        practice: 'Healthcare',
+        hospital: 'Kitui Hospital',
+        work_experience: 29,
+        price: 9000,
+        image: doctor10,
+        county: 'Kitui',
+        contact: '+2547919471029'
+
+    },
+    {
+        id: '21',
+        name: 'Dr. Joseph Kiprop',
+        specialty: 'Mental Health Nursing',
+        practice: 'Mental Health',
+        hospital: 'Mombasa Youth Centre',
+        work_experience: 17,
+        price: 7000,
+        image: doctor21,
+        county: 'Mombasa',
+        contact: '+2547809890109'
+
+    },
+
+]
