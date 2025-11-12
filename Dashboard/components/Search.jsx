a6b8d77063af4a0ad08a6930d0ebd7f04b7aef96
Mwia <iansnr10@gmail.com>
2025-11-12 10:18:27 +0300

    preventing debounce search

--- Dashboard/components/Search.jsx
+++ Dashboard/components/Search.jsx
@@ -1,13 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { SearchContext } from '../../src/context/SearchCreator';
 
 export default function Search(){
-    const {searchTerm, setSearchTerm} = useContext(SearchContext)
+    const {setSearchTerm} = useContext(SearchContext)
+    const [input, setInput] = useState('')
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault()
+            setSearchTerm(input)
+        }
+  };
 
     return(
         <>
-        <div className='mb-2'>
-            <input className='outline p-2' type="text" placeholder='Search Doctors by name, specialty or practice' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}/>
+        <div className='mb-2 p-2'>
+            <input className='outline p-2 rounded w-full max-w-sm' type="text" placeholder='Search Doctors by name, specialty or practice' value={input} onChange={(e)=> setInput(e.target.value)} onKeyDown={handleKeyDown}/>
         </div>
         </>
     )
