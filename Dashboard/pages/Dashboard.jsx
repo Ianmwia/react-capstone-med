409c4a6d03ea91a21c394d58717e01f7a156a682
Mwia <iansnr10@gmail.com>
2025-11-12 10:20:21 +0300

    adding a navigate button

--- Dashboard/pages/Dashboard.jsx
+++ Dashboard/pages/Dashboard.jsx
@@ -5,9 +5,11 @@ import Card from '../components/Card';
 import Search from '../components/Search';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import { useNavigate } from 'react-router-dom';
 
 export default function Dashboard(){
     const {searchTerm} = useContext(SearchContext)
+    const navigate = useNavigate()
 
     const [filterDoctors, setFilterDoctors] = useState(doctors)
 
@@ -23,8 +25,11 @@ export default function Dashboard(){
     return(
         <>
         <Navbar/>
-        <div>
+        <div className='flex justify-between'>
             <Search/>
+            <button 
+            className='p-2 bg-green-400 text-white font semibold rounded m-2 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:underline' 
+            onClick={()=> navigate('/bookings')}>Bookings Tab</button>
         </div>
         <main className='m-2 '>
         <div>
