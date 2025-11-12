ebde618c5a48ee3374db0052e23b938de7eaadec
Mwia <iansnr10@gmail.com>
2025-11-12 13:28:33 +0300

    styling the login and sign up pages

--- pages/LoginPage.jsx
+++ pages/LoginPage.jsx
@@ -1,9 +1,14 @@
 import LoginForm from '../components/LoginForm';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login(){
+    const navigate = useNavigate()
     return(
         <>
-        <LoginForm/>
+        <div className='flex flex-col items-center justify-center min-h-screen px-4 gap-8 bg-gray-300'>
+            <LoginForm/>
+        <p className='text-black'>Don't have an Account? <span onClick={()=> navigate('/signup')} className='text-green-500 font-semibold cursor-pointer'>Sign Up</span></p>
+        </div>
         </>
     )
 }
--- pages/SignupPage.jsx
+++ pages/SignupPage.jsx
@@ -3,7 +3,9 @@ import SignupForm from '../components/SignupForm'
 export default function Signup(){
     return(
         <>
+        <div className='flex flex-col items-center justify-center min-h-screen px-4 gap-8 bg-gray-300'>
         <SignupForm/>
+        </div>
         </>
     )
 }
