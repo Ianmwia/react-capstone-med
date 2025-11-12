27eda018e3ecbbb41d5feeea2773bd82158ab3eb
Mwia <iansnr10@gmail.com>
2025-11-12 13:24:30 +0300

    styling the login and sign up pages

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,7 +4,7 @@ import { login } from '../src/slices/authSlice'
 import { useNavigate } from 'react-router-dom'
 
 export default function LoginForm(){
-    const { user, status} = useSelector((state)=> state.auth)
+    const { user, error} = useSelector((state)=> state.auth)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch()
@@ -16,25 +16,22 @@ export default function LoginForm(){
     }
     //switch from visitor to dashboard
     useEffect(()=>{
-        if(status){
-            if(status === 'loggedIn'){
-                navigate('/dashboard')
-            }else if(status === 'loggedOut'){
-                navigate('/')
-            }
+        if(user){
+            navigate('/dashboard')
         }
-    },[user, status, navigate])
+    }, [user, navigate])
 
     return(
         <>
-        <div className='flex items-center justify-center min-h-screen bg-gray-300'>
+        <div className='flex items-center justify-center'>
             <div className='bg-white p-7 rounded-xl w-96 flex flex-col'>
                 <h2 className='text-[20px] font-semibold text-center mb-4'>Login</h2>
                 <form onSubmit={handleLogin} className='flex flex-col flex-1 gap-4'>
                     <input className='outline p-2 rounded' type="email" placeholder='email' value={email} autoComplete='email' onChange={(e)=> setEmail(e.target.value)}/>
                     <input className='outline p-2 rounded' type='password' placeholder='password' value={password} autoComplete='current-password' onChange={(e)=> setPassword(e.target.value)}/>
-                    <button type='submit' className='bg-green-400 text-white p-2 rounded-lg cursor-pointer'>Login</button>
+                    <button type='submit' className='bg-green-400 text-white p-2 rounded-lg cursor-pointer font-semibold hover:bg-green-600'>Login</button>
                 </form>
+                {error && <p className='text-red-500 p-2 text-center'>{error}</p>}
             </div>
         </div>
         </>
--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -4,7 +4,7 @@ import { signupUser } from '../src/slices/authSlice'
 import { useNavigate } from 'react-router-dom'
 
 export default function SignupForm(){
-    const { user, status} = useSelector((state)=> state.auth)
+    const { user, status, error} = useSelector((state)=> state.auth)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch()
@@ -25,14 +25,15 @@ export default function SignupForm(){
 
     return(
         <>
-        <div className='flex items-center justify-center min-h-screen bg-gray-300'>
+        <div className='flex items-center justify-center'>
             <div className='bg-white p-7 rounded-xl w-96 flex flex-col'>
                 <h2 className='text-[20px] font-semibold text-center mb-4'>Sign Up</h2>
                 <form onSubmit={handleSignup} className='flex flex-col flex-1 gap-4'>
                     <input className='outline p-2 rounded' type="email" placeholder='email' value={email} autoComplete='email' onChange={(e)=> setEmail(e.target.value)}/>
                     <input className='outline p-2 rounded' type='password' placeholder='password' value={password} autoComplete='current-password' onChange={(e)=> setPassword(e.target.value)}/>
-                    <button type='submit' className='bg-green-400 text-white p-2 rounded-lg cursor-pointer'>Sign Up</button>
+                    <button type='submit' className='bg-green-400 text-white p-2 rounded-lg cursor-pointer font-semibold hover:bg-green-600'>Sign Up</button>
                 </form>
+                {error && <p className='text-red-500 p-2 text-center'>{error}</p>}
             </div>
         </div>
         </>
